8201cb7a2bb94855435c68552ead39f9fdb6568f
Adam Pan <adam.pan@live.com>
2013-01-11 20:36:05 -0500

    How to remove duplicate string with and without Linq.

---
+++ zocdoc/removeDuplicate.cs
@@ -0,0 +1,23 @@
+#without using Linq
+public List<String> removeDuplicate(List<String> input)
+{
+	#this implementation has time complexity of O(2n) and space complexity of O(2n)
+	#which is still better than pure List<String> implementation.
+	List<String> returnList = new List<String>();
+	Hashtable ht = new Hashtable();
+	
+	foreach (String s in input)
+	{
+		if (!ht.ContainsKey(s))
+		{
+			ht.Add(s, "");
+			returnList.Add(s);
+		}
+	}
+	
+	return returnList;
+}
+
+#using Linq
+input.GroupBy(x => x).Where(group => group.Count() > 1).Select(group => Group.Key).ToList();
+#easy huh? :)

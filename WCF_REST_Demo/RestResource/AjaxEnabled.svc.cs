f6dd9293c702b00ad2cda66e1551956879bf4d06
Adam Pan <adam.pan@live.com>
2013-01-14 09:35:17 -0500

    Demo app for REST service in WCF.

---
+++ WCF_REST_Demo/RestResource/AjaxEnabled.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace RestResource
+{
+  [ServiceContract(Namespace = "")]
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+  public class AjaxEnabled
+  {
+    [OperationContract]
+    public string SayHello(string name)
+    {
+      return string.Format("Hello, {0}", name);
+    }
+  }
+}

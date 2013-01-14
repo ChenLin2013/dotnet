d625fccf8be976f8245f74eec9a42ff57fb1c606
Adam Pan <adam.pan@live.com>
2013-01-14 09:31:07 -0500

    Demo app for REST service in WCF.

---
+++ WCF_REST_Demo/RestResource/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RestResource.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+      <Services>
+        <asp:ServiceReference Path="~/AjaxEnabled.svc" />
+      </Services>
+      <Scripts>
+        <asp:ScriptReference Path="~/Scripts/jquery-1.4.1.js" />
+      </Scripts>
+    </asp:ScriptManager>
+    <script type="text/javascript">
+      function SayHi(nameId, helloResultId) {
+        var name = $(nameId)[0].value;
+        var ajaxCall = new AjaxEnabled();
+        ajaxCall.SayHello(name,
+          function(result) {
+            $(helloResultId).html(result);
+          },
+
+          function(result) {
+            alert("Failed: " + result);
+          });
+      }
+    </script>
+    <div>
+      What is your name? <input id="name" maxlength="25" />
+      <input type="button" onclick="SayHi('#name', '#helloResult');" value="Say Hi"/>
+    </div>
+    <div id="helloResult"></div>
+    </form>
+</body>
+</html>
---
+++ WCF_REST_Demo/RestResource/LookupTable.svc.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace RestResource
+{
+  [ServiceContract]
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+  public class LookupTable
+  {
+
+    static readonly Dictionary<string, int> StoredValues = new Dictionary<string, int>();
+
+    [WebInvoke(Method = "POST", UriTemplate = "{name}")]
+    [OperationContract]
+    public void CreateStoredValue(string name, int value)
+    {
+      int retval;
+      if (!StoredValues.TryGetValue(name, out retval))
+      {
+        StoredValues[name] = value;
+        return;
+      }
+      var error = string.Format("Value named {0} already exists. Can not create new value with the same name. Did you mean to use POST?", name);
+      throw new WebFaultException<string>(error, HttpStatusCode.Conflict);
+    }
+
+    [WebGet(UriTemplate = "{name}")]
+    [OperationContract]
+    public int GetStoredValue(string name)
+    {
+      int retval;
+      if (StoredValues.TryGetValue(name, out retval))
+      {
+        return retval;
+      }
+
+      var error = string.Format("Value named {0} could not be found.", name);
+      throw new WebFaultException<string>(error, HttpStatusCode.NotFound);
+    }
+
+    [WebInvoke(Method = "PUT", UriTemplate = "{name}")]
+    [OperationContract]
+    public void UpdateStoredValue(string name, int value)
+    {
+      int retval;
+      if (StoredValues.TryGetValue(name, out retval))
+      {
+        StoredValues[name] = value;
+        return;
+      }
+      var error = string.Format("Value named {0} could not be found.", name);
+      throw new WebFaultException<string>(error, HttpStatusCode.NotFound);
+    }
+
+    [WebInvoke(Method = "DELETE", UriTemplate = "{name}")]
+    [OperationContract]
+    public void DeleteStoredValue(string name)
+    {
+      int retval;
+      if (StoredValues.TryGetValue(name, out retval))
+      {
+        StoredValues.Remove(name);
+      }
+    }
+  }
+}
